d836432036b88d9bb607cc6b9378b328befce88f
Pavel Tagiltsev <63656106+pavel-tagiltsev@users.noreply.github.com>
2021-06-01 22:25:07 +0300

    Uploading the project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.about__slider, .route__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+    autoplay: true,
+    fade: true,
+    responsive: [{
+      breakpoint: 601,
+      settings: {
+        arrows: false
+      }
+    }, ]
+  });
+});
